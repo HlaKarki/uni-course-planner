944abd89a834cf30272e123c4f07fe1410d9c451
Hla Htun <kshap.karki@gmail.com>
2023-05-22 21:44:37 -0700

    serving professors page, schedules page

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.get('/', (req, res) => {
     res.status(200).sendFile(__dirname+'/public/index.html')
 })
 
-app.get('/studentsPage', (req,res,next) => {
+app.get('/students', (req,res,next) => {
     res.status(200).render('studentsPage')
 })
 
@@ -65,7 +65,78 @@ app.get("/courseDetails/:courseId", (req, res, next) => {
     let courseName = ""
 
     res.status(200).render('courseDetailsPage', {
-        courseName: "Test title"
+        courseName: "Test course title",
+        online: 0
+    })
+})
+
+app.get("/professors", (req, res, next) => {
+    res.status(200).render('professorsPage')
+})
+
+app.get("/classrooms", (req, res, next) => {
+    res.status(200).render('classroomsPage')
+})
+
+app.get("/courses", (req, res, next) => {
+    const courses = [
+        {
+            id: 1,
+            name: "Intro To Assembly",
+            instructor: "Paris Kalathas",
+            totalEnrolled: 1,
+            online: 0,
+            time: "17:00 MWs",
+            location: "Weniger 153"
+        },
+        {
+            id: 2,
+            name: "Intro To Database",
+            instructor: "Christopher Buss",
+            totalEnrolled: 1,
+            online: 0,
+            time: "08:00 MWs",
+            location: "Weniger 151"
+        },
+        {
+            id: 3,
+            name: "Web Development",
+            instructor: "Robb Hess",
+            totalEnrolled: 1,
+            online: 1
+        }
+    ]
+    res.status(200).render('coursesPage', {
+        courses
+    })
+})
+
+app.get("/schedules", (req, res, next) => {
+    const schedules = [
+        {
+            id: 1,
+            idStudent: 1,
+            username: "cameron",
+            credits: 16,
+            term: "Spring 2023"
+        },
+        {
+            id: 2,
+            idStudent: 2,
+            username: "hla",
+            credits: 20,
+            term: "Spring 2023"
+        },
+        {
+            id: 3,
+            idStudent: 3,
+            username: "trenton",
+            credits: 22,
+            term: "Spring 2023"
+        }
+    ]
+    res.status(200).render('schedulesPage', {
+        schedules
     })
 })
 
