99887b1e950e1122325c5d9ba837ce0dba92eb68
Hla Htun <kshap.karki@gmail.com>
2023-05-22 20:40:48 -0700

    serving students page, studentSchedules page, studentCourses page, courseDetails page

--- server.js
+++ server.js
@@ -13,19 +13,62 @@ app.set('view engine', 'handlebars')
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-    res.status(200).sendFile(__dirname+'/public/landingPage/index.html')
+    res.status(200).sendFile(__dirname+'/public/index.html')
 })
 
-app.get('/student/studentsPage/:studentID', (req, res, next) => {
-    console.log("IN HEREEEEEEEEEEEE")
-    const student = req.params.studentID
-    console.log(student)
-    console.log("== Student id: ", student)
-
+app.get('/studentsPage', (req,res,next) => {
     res.status(200).render('studentsPage')
-    // res.status(200).sendFile(__dirname+'/views/student/studentsPage.handlebars')
-    // res.status(200).render('studentsPage')
 })
+
+app.get('/studentSchedules/:studentID', (req, res, next) => {
+    const studentId = parseInt(req.params.studentID, 10)
+    let studentName = ""
+    if (studentId === 1){
+        studentName = "Cameron"
+    }
+    else if (studentId === 2){
+        studentName = "Hla"
+    }
+    else if (studentId === 3){
+        studentName = "Trenton"
+    }
+    console.log("== Student id: ", studentId)
+
+    res.status(200).render('studentSchedulesPage', {
+        studentName: studentName,
+        studentId: studentId
+    })
+})
+
+app.get('/studentCourses/:studentId', (req, res, next) => {
+    const studentId = parseInt(req.params.studentId, 10)
+    let studentName = ""
+    if (studentId === 1){
+        studentName = "Cameron"
+    }
+    else if (studentId === 2){
+        studentName = "Hla"
+    }
+    else if (studentId === 3){
+        studentName = "Trenton"
+    }
+    console.log("== Student id from courses: ", studentId)
+
+    res.status(200).render('studentCoursesPage', {
+        studentName: studentName,
+        term: "Spring 2023"
+    })
+})
+
+app.get("/courseDetails/:courseId", (req, res, next) => {
+    const courseId = parseInt(req.params.courseId, 10)
+    let courseName = ""
+
+    res.status(200).render('courseDetailsPage', {
+        courseName: "Test title"
+    })
+})
+
 app.listen(port, (err) => {
     if (err) { throw err }
     console.log("== Server listening on port", port)
