c7ede0cb9762c1f96bb204a623ae0296944880c2
Hla Htun <kshap.karki@gmail.com>
2023-05-25 18:54:26 -0700

    server now calls all the get and post methods from the designated CRUD.js files

--- server.js
+++ server.js
@@ -7,7 +7,9 @@ const app = express()
 
 const CRUDStudents = require('./CRUDStudents')
 const CRUDProfessors = require('./CRUDProfessors')
-
+const CRUDCourses = require('./CRUDCourses')
+const CRUDClassrooms = require('./CRUDClassrooms')
+const CRUDSchedules = require('./CRUDSchedules')
 
 app.engine('handlebars', exphbs.engine({
     defaultLayout: "main"
@@ -19,169 +21,35 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Students Entity CRUD queries
-CRUDStudents.addStudent(app, db)        // CREATE
-CRUDStudents.getStudent(app, db)        // READ
-CRUDStudents.updateStudent(app, db)     // UPDATE
-CRUDStudents.deleteStudent(app, db)     // DELETE
+CRUDStudents.addStudent(app, db)            // CREATE
+CRUDStudents.getStudent(app, db)            // READ
+CRUDStudents.updateStudent(app, db)         // UPDATE
+CRUDStudents.deleteStudent(app, db)         // DELETE
 
-
+// Gets a specific student's schedules
 CRUDStudents.getStudentSchedules(app, db)
 
-app.get('/studentCourses/:studentId', (req, res, next) => {
-    const studentId = parseInt(req.params.studentId, 10)
-    let studentName = ""
-    if (studentId === 1){
-        studentName = "Cameron"
-    }
-    else if (studentId === 2){
-        studentName = "Hla"
-    }
-    else if (studentId === 3){
-        studentName = "Trenton"
-    }
-    console.log("== Student id from courses: ", studentId)
+// Gets a specific student's schedule's courses
+CRUDStudents.getStudentCourses(app, db)
 
-    // 1, Intro to assembly, , 4, CS 162, A-
-    const studentCourses = [
-        {
-            courseId: 1,
-            title: "Intro to Assembly",
-            description: "Learning IA32, MASM and CISC Architecture",
-            credits: 4,
-            prereq: "CS 162",
-            grade: "A-"
-        }
-    ]
-    res.status(200).render('studentCoursesPage', {
-        studentName: studentName,
-        term: "Spring 2023",
-        studentCourses
-    })
-})
-
-app.get("/courseDetails/:courseId", (req, res, next) => {
-    const courseId = parseInt(req.params.courseId, 10)
-    let courseTitle = ""
-    const courseDetails = [
-        {
-            courseId: courseId,
-            title: "Intro to Assembly",
-            professor: "Paris Kalathas",
-            totalEnrolled: 1,
-            online: 0,
-            meetTime: "17:00 MWs",
-            location: "Weniger 153"
-        },
-        {
-            courseId: courseId,
-            title: "Intro to Database",
-            professor: "Christopher Buss",
-            totalEnrolled: 1,
-            online: 0,
-            meetTime: "08:00 MWs",
-            location: "Weniger 151"
-        }
-    ]
-
-    res.status(200).render('courseDetailsPage', {
-        courseName: "Test course title",
-        courseDetails
-    })
-})
+// Gets a specific course's details
+CRUDCourses.getCourseDetails(app, db)
 
 // Professors Entity CRUD queries
-CRUDProfessors.getProfessors(app, db)
-CRUDProfessors.addProfessor(app, db)
-CRUDProfessors.deleteProfessor(app, db)
-CRUDProfessors.updateProfessor(app, db)
+CRUDProfessors.getProfessors(app, db)       // READ
+CRUDProfessors.addProfessor(app, db)        // CREATE
+CRUDProfessors.deleteProfessor(app, db)     // DELETE
+CRUDProfessors.updateProfessor(app, db)     // UPDATE
 
-app.get("/classrooms", (req, res, next) => {
-    const classrooms = [
-        {
-            classroomId: 1,
-            totalSeats: 60,
-            building: "Weniger",
-            roomNumber: 153
-        },
-        {
-            classroomId: 2,
-            totalSeats: 150,
-            building: "Weniger",
-            roomNumber: 151
-        },
-        {
-            classroomId: 3,
-            totalSeats: 105,
-            building: "Linus Pauling Science Center",
-            roomNumber: 125
-        }
-    ]
-    res.status(200).render('classroomsPage', {
-        classrooms
-    })
-})
+// Gets all the classrooms in the database
+CRUDClassrooms.getClassrooms(app, db)
 
-app.get("/courses", (req, res, next) => {
-    const courses = [
-        {
-            id: 1,
-            name: "Intro To Assembly",
-            instructor: "Paris Kalathas",
-            totalEnrolled: 1,
-            online: 0,
-            time: "17:00 MWs",
-            location: "Weniger 153"
-        },
-        {
-            id: 2,
-            name: "Intro To Database",
-            instructor: "Christopher Buss",
-            totalEnrolled: 1,
-            online: 0,
-            time: "08:00 MWs",
-            location: "Weniger 151"
-        },
-        {
-            id: 3,
-            name: "Web Development",
-            instructor: "Robb Hess",
-            totalEnrolled: 1,
-            online: 1
-        }
-    ]
-    res.status(200).render('coursesPage', {
-        courses
-    })
-})
+// Gets all the courses in the database
+CRUDCourses.getCourses(app, db)
 
-app.get("/schedules", (req, res, next) => {
-    const schedules = [
-        {
-            id: 1,
-            idStudent: 1,
-            username: "cameron",
-            credits: 16,
-            term: "Spring 2023"
-        },
-        {
-            id: 2,
-            idStudent: 2,
-            username: "hla",
-            credits: 20,
-            term: "Spring 2023"
-        },
-        {
-            id: 3,
-            idStudent: 3,
-            username: "trenton",
-            credits: 22,
-            term: "Spring 2023"
-        }
-    ]
-    res.status(200).render('schedulesPage', {
-        schedules
-    })
-})
+// Gets all the schedules in the database
+CRUDSchedules.getSchedules(app, db)
+
 
 app.listen(port, (err) => {
     if (err) { throw err }
