a098de8850f0c777a2df2383e3facc0177d2ee8c
Hla Htun <kshap.karki@gmail.com>
2023-06-01 14:25:04 -0700

    delete schedule operation

--- CRUDs/CRUDSchedules.js
+++ CRUDs/CRUDSchedules.js
@@ -28,3 +28,17 @@ module.exports.getSchedules = (app, db) => {
         })
     )
 }
+
+module.exports.deleteSchedule = (app, db) => {
+    return (
+        app.post("/deleteSchedule", (req, res, next) => {
+            const form_input = req.body
+
+            const deleteSchedule = `DELETE FROM Schedules WHERE idSchedule = ${form_input["scheduleDelete"]};`
+
+            db.pool.query(deleteSchedule, (err, recSchedules, fields) => {
+                err ? res.sendStatus(400) : res.redirect('/schedules')
+            })
+        })
+    )
+}
